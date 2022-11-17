8ef3cc04fbfd5cf28bf0fdf7484fd5b2d2de6e3d
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-11-17 16:10:09 -0300

    ⚡ Add comments

--- problems/array/matchingbrackets.go
+++ problems/array/matchingbrackets.go
@@ -19,30 +19,39 @@ func ValidateParenthesisStack(input string) bool {
 		return true
 	}
 
+	// keep count of the open parenthesis and the asterisks
+	// TODO: instead of a stack, we can keep count of both variables as int
 	openParenthesisStack := ds.NewStack[struct{}]()
 	asterisksStack := ds.NewStack[struct{}]()
 
 	for _, char := range input {
 		switch string(char) {
 		case ")":
+			// check if we have an open parenthesis to close
 			if _, err := openParenthesisStack.Pop(); err == nil {
 				break
 			}
 
+			// check if we can use an asterisks to close with this parenthesis
 			if _, err := asterisksStack.Pop(); err == nil {
 				break
 			}
 
+			// we do not have enough parenthesis or asterisks to close
 			return false
 
 		case "(":
+			// increase the open parenthesis count
 			openParenthesisStack.Push(struct{}{})
 		case "*":
+			// increase the asterisks count
 			asterisksStack.Push(struct{}{})
 		}
 	}
 
+	// check if we have enough asterisks to close the open parenthesis left
 	canCloseOpenParenthesis := asterisksStack.Len()-openParenthesisStack.Len() >= 0
 
+	// check if we have some open parenthesis left
 	return openParenthesisStack.IsEmpty() || canCloseOpenParenthesis
 }
